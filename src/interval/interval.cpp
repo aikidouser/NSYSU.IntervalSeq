0f1bdacb7cfa2b996c0d339e20d41a9aa287c2d4
aikidouser <t36085520@icloud.com>
2023-04-09 14:54:42 +0800

    feat(interval): Add the pointer prev for trace back

--- src/interval/interval.cpp
+++ src/interval/interval.cpp
@@ -3,12 +3,16 @@
 
 #include "interval.hpp"
 
+using std::shared_ptr;
+
 interval::interval(int start, int end) : s(start), e(end), l(e - s + 1) {
   if (l < 1) {
     throw std::invalid_argument(
         "The start of the interval is bigger than the end.");
   }
 }
+
+void interval::set_prev(const shared_ptr<interval> &prev) { this->prev = prev; }
 
 bool operator<(const interval &x, const interval &y) {
   return x.s < y.s && x.e < y.e;
--- src/interval/interval.hpp
+++ src/interval/interval.hpp
@@ -1,14 +1,22 @@
 #ifndef __INTERVAL_HPP__
 #define __INTERVAL_HPP__
 
+#include <memory>
+
 // Full Interval
 class interval {
+private:
+  std::shared_ptr<interval> prev;
+
 public:
   const int s;
   const int e;
   const int l;
+
   interval(int start, int end);
   ~interval() {}
+
+  void set_prev(const std::shared_ptr<interval> &prev);
 };
 
 bool operator<(const interval &x, const interval &y);
